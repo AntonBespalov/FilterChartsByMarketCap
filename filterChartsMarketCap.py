13efb2b0e4c629b3640fec192db59982d3676ff5
AntonBespalov <bespalov.anton@gmail.com>
2021-03-31 12:00:27 +0300

    Добавил фильтрацию по цене акции (удаление файлов, цена акций на которых больше заданной)

--- filterChartsMarketCap.py
+++ filterChartsMarketCap.py
@@ -19,50 +19,77 @@ if max_cap.isnumeric():
     max_cap_bln = float(max_cap)
     max_cap_is_defined = True
 
+max_price_str = config.get('settings', 'max_price')
+max_price_is_defined = False
+if max_price_str.isnumeric():
+    max_price = float(max_price_str)
+    max_price_is_defined = True
+
 rename_files = config.get('settings', 'rename_files').lower()
 path_to_charts = config.get('path', 'path')
 
 print("формирование списка тикеров")
 tickers_list = []
 directory = Path(path_to_charts)
+
 for root, _, f_names in os.walk(directory):
     for f in f_names:
         if f.endswith('.png'):
             if 'FAANG-FAAMG + other stocks' not in root:
                 continue
 
-            ticker = f.split('_')[0]
+            filename_splitted = f.split('_')
+            if len(filename_splitted) > 2:
+                # в названии тикера есть подчеркивание (пример: BRK_B), заменяем на дефис
+                ticker = filename_splitted[0] + '-' + filename_splitted[1]
+            else:
+                ticker = filename_splitted[0]
             tickers_list.append(ticker)
 
 print("получение данных по капитализации")
 start = time.time()
 try:
-    cap = web.get_quote_yahoo(tickers_list)['marketCap']
-except KeyError:
-    exit(1)
+    quote = web.get_quote_yahoo(tickers_list)
+    cap = quote['marketCap']
+    price = quote['regularMarketPrice']
+except Exception as ex:
+    print('Error:', ex)
 end = time.time()
 elapsed_time = format((end - start), '.2f')
 print(f'данные по {len(tickers_list)} тикерам получены за {elapsed_time} сек.')
 
-print("удаление файлов акций, которые не подходят под заданные параметры капитализации")
-deleted_tickers = dict()
+print("удаление файлов акций, которые не подходят под заданные параметры капитализации и цены")
+del_tickers_cap = dict()
+del_tickers_price = dict()
 for root, _, f_names in os.walk(directory):
     for f in f_names:
         if f.endswith('.png'):
             if 'FAANG-FAAMG + other stocks' not in root:
                 continue
 
-            ticker = f.split('_')[0]
+            filename_splitted = f.split('_')
+            if len(filename_splitted) > 2:
+                # в названии тикера есть подчеркивание (пример: BRK_B), заменяем на дефис
+                ticker = filename_splitted[0] + '-' + filename_splitted[1]
+            else:
+                ticker = filename_splitted[0]
+
             cap_bln = float(format((cap[ticker] / 1e9), '.2f'))
 
             if (max_cap_is_defined and cap_bln > max_cap_bln) or \
                     (min_cap_is_defined and cap_bln < min_cap_bln):
                 os.remove(os.path.join(root, f))
-                deleted_tickers[ticker] = cap_bln
+                del_tickers_cap[ticker] = cap_bln
+                continue
+
+            if price[ticker] > max_price:
+                os.remove(os.path.join(root, f))
+                del_tickers_price[ticker] = price[ticker]
                 continue
 
             if rename_files == 'yes':
                 new_name = str(cap_bln) + '_' + f
                 os.rename(os.path.join(root, f), os.path.join(root, new_name))
 
-print(f'тикеры удаленных файлов с капитализацией: {str(dict(deleted_tickers))}')
+print(f'тикеры файлов, не подошедших по капитализации компании: {str(dict(del_tickers_cap))}')
+print(f'тикеры файлов, не подошедших по цене акции: {str(dict(del_tickers_price))}')
